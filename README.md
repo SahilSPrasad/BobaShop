22694dd7b062189be61b8f9577663d5ecfff5b7c
SahilSPrasad <SahilSPrasad@github.com>
2022-12-01 01:41:54 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+CSC 174 Deliverable 3
---
+++ client/src/App.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import Axios from "axios";
+
+function App() {
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [customerName, setCustomerName] = useState("");
+
+  const handleSubmit = (e) => {
+    Axios.post("https://bobashop-174.herokuapp.com//register", {
+      phoneNumber: phoneNumber,
+      customerName: customerName,
+    }).then((response) => {
+      console.log(response);
+    });
+  };
+
+  return (
+    <div className="App">
+      <h1>Boba Shop</h1>
+      <p>Enter your account information</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input
+            onChange={(e) => setCustomerName(e.target.value)}
+            value={customerName}
+          ></input>
+        </label>
+        <br />
+        <label>
+          Phone Number:
+          <input
+            name="phone number"
+            onChange={(e) => setPhoneNumber(e.target.value)}
+            value={phoneNumber}
+          ></input>
+        </label>
+        <br />
+        <button type="submit">submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
